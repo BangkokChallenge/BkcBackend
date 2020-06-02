32aff48557031748b2a54ee9f6f207caf35877ff
chohongjae <bjq913@naver.com>
2020-06-03 02:20:00 +0900

    해쉬태그를 이용해서 해당 post 리턴하기 기능 추가

--- src/main/java/us/dev/backend/Post/PostRepository.java
+++ src/main/java/us/dev/backend/Post/PostRepository.java
@@ -1,12 +1,9 @@
 package us.dev.backend.Post;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import us.dev.backend.Account.Account;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface PostRepository extends JpaRepository<Post, Integer> {
@@ -15,5 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findAllByOrderByCreatedAtDesc();
     /* 내가 작성한 Post 검색 (생성날짜 내림차순) */
     List<Post> findByAccountIdOrderByCreatedAtDesc(String accountId);
-
+    // 해쉬태그를 이용해서 해당 post 리턴하기 (생성날짜 내림차순)
+    List<Post> findAllByHashTagsContentOrderByCreatedAtDesc(String content);
 }
